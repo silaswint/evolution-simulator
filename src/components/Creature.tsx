8fba59906e3a2221352c44af5a75bbee3730c57a
silas.winter <silas.winter@p2media.de>
2024-01-17 23:56:23 +0100

    [TASK] don't let the creatures run out of the map

--- src/components/Creature.tsx
+++ src/components/Creature.tsx
@@ -1,6 +1,7 @@
 // src/components/Creature.tsx
 import React, { useState, useEffect } from 'react';
 import { generateRandomGenome } from '@/utils/genomeUtils';
+import {evolutionConfig} from "@/utils/evolutionConfig";
 
 interface CreatureProps {
     initialPosition: { x: number; y: number };
@@ -14,32 +15,44 @@ const Creature: React.FC<CreatureProps> = ({ initialPosition, secondsLeftForCurr
     useEffect(() => {
         const moveCreature = () => {
             if (secondsLeftForCurrentGeneration > 0) {
-                const movementDirection = genome.substr(0, 1); // Annahme: Das erste Gen bestimmt die Bewegungsrichtung
+                const movementDirection = genome.substr(0, 1);
+
+                // Kopiere die aktuelle Position, um die Änderungen zu überwachen
+                let newPosition = { ...position };
 
                 switch (movementDirection) {
                     case '0':
-                        setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x - 1 }));
+                        newPosition.x = Math.max(0, newPosition.x - 1); // Linke Grenze
                         break;
                     case '1':
-                        setPosition((prevPosition) => ({ ...prevPosition, x: prevPosition.x + 1 }));
+                        newPosition.x = Math.min(evolutionConfig.mapSize.width - 10, newPosition.x + 1); // Rechte Grenze
                         break;
                     case '2':
-                        setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y - 1 }));
+                        newPosition.y = Math.max(0, newPosition.y - 1); // Obere Grenze
                         break;
                     case '3':
-                        setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y + 1 }));
+                        newPosition.y = Math.min(evolutionConfig.mapSize.height - 10, newPosition.y + 1); // Untere Grenze
                         break;
                     default:
                     // Keine Bewegung
                 }
+
+                // Setze die neue Position nur, wenn sie innerhalb der Grenzen liegt
+                if (
+                    newPosition.x >= 0 &&
+                    newPosition.x <= evolutionConfig.mapSize.width - 10 &&
+                    newPosition.y >= 0 &&
+                    newPosition.y <= evolutionConfig.mapSize.height - 10
+                ) {
+                    setPosition(newPosition);
+                }
             }
         };
 
-        const moveInterval = setInterval(moveCreature, 1); // Annahme: Bewegung alle 1 Millisekunde
+        const moveInterval = setInterval(moveCreature, 1);
 
-        // Cleanup-Funktion für die Aufhebung des Intervalls beim Entfernen der Komponente
         return () => clearInterval(moveInterval);
-    }, [genome, secondsLeftForCurrentGeneration]);
+    }, [genome, secondsLeftForCurrentGeneration, position]);
 
     return (
         <div
@@ -49,7 +62,7 @@ const Creature: React.FC<CreatureProps> = ({ initialPosition, secondsLeftForCurr
                 top: `${position.y}px`,
                 width: '10px',
                 height: '10px',
-                backgroundColor: '#00F', // Beispiel: Blaue Farbe für die Kreatur
+                backgroundColor: '#00F',
             }}
             data-genome={genome}
         >
