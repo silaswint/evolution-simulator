cc081c9026935966ac4ac9ca959e52f1cf78750a
silas.winter <silas.winter@p2media.de>
2024-01-19 13:02:01 +0100

    [TEST] hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,16 @@
-// src/App.tsx
-import React, { useState, useEffect, useRef } from 'react'
-import EvolutionMap from './components/EvolutionMap'
-import { evolutionConfig } from './utils/evolutionConfig'
+import { BlurFilter } from 'pixi.js'
+import { Stage, Container, Text } from '@pixi/react'
+import React, { useMemo } from 'react'
 
-const App: React.FC = () => {
-  const [generation, setGeneration] = useState<number>(0)
-  const [population, setPopulation] = useState<number>(evolutionConfig.population)
-  const [secondsPerGeneration, setSecondsPerGeneration] = useState<number>(evolutionConfig.secondsPerGeneration)
-  const [secondsLeftForCurrentGeneration, setSecondsLeftForCurrentGeneration] = useState<number>(secondsPerGeneration)
-
-  // Verwende useRef, um den aktuellen Wert von secondsLeftForCurrentGeneration zu speichern
-  const secondsLeftRef = useRef(secondsLeftForCurrentGeneration)
-  secondsLeftRef.current = secondsLeftForCurrentGeneration
-
-  const populationRef = useRef(population)
-  populationRef.current = population
-
-  const generationRef = useRef(generation)
-  generationRef.current = generation
-
-  useEffect(() => {
-    // Starte den Timer, um die verbleibenden Sekunden zu aktualisieren
-    const timerInterval = setInterval(() => {
-      setSecondsLeftForCurrentGeneration((prevSeconds) => Math.max(0, prevSeconds - 1))
-    }, 1000)
-
-    // Cleanup-Funktion für die Aufhebung des Intervalls beim Entfernen der Komponente
-    return () => {
-      clearInterval(timerInterval)
-    }
-  }, [secondsPerGeneration])
-
-  const handleNextGeneration = (): void => {
-    // Hier kannst du Logik für den Übergang zur nächsten Generation hinzufügen
-    // Beispiel: Implementiere Reproduktion, Mutation, Selektion
-    setGeneration((prevGeneration) => prevGeneration + 1)
-  }
+export const App: React.FC = () => {
+  const blurFilter = useMemo(() => new BlurFilter(4), [])
 
   return (
-        <div>
-            <h1>Evolution Simulation</h1>
-            <p>Generation: {generation}</p>
-            <p>Seconds left for current generation: {secondsLeftForCurrentGeneration}</p>
-            <button onClick={handleNextGeneration}>Next Generation</button>
-            <EvolutionMap secondsLeftRef={secondsLeftRef} populationRef={populationRef} generationRef={generationRef} />
-        </div>
+        <Stage options={{ backgroundColor: 0xeef1f5 }}>
+            <Container x={400} y={330} >
+                <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} filters={[blurFilter]} />
+            </Container>
+        </Stage>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 // src/index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const rootElement = document.getElementById('root');
-if (rootElement == null) throw new Error('Failed to find the root element');
-const root = ReactDOM.createRoot(rootElement);
+const rootElement = document.getElementById('root')
+if (rootElement == null) throw new Error('Failed to find the root element')
+const root = ReactDOM.createRoot(rootElement)
 
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
-);
+)
