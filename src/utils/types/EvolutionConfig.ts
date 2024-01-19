876cff93dd8921053bc80c4673b2d244e7b958c8
silas.winter <silas.winter@p2media.de>
2024-01-19 19:05:10 +0100

    [TEST] fix EvolutionConfig.ts types

--- src/utils/types/EvolutionConfig.ts
+++ src/utils/types/EvolutionConfig.ts
@@ -3,9 +3,9 @@ import { type MapSize } from '@/utils/types/MapSIze'
 export interface EvolutionConfig {
   population: number // Anzahl der Kreaturen in der Population
   mapSize: MapSize // Größe der Karte in Pixeln
-  secondsPerGeneration: 10 // Schritte pro Generation
-  genomeSize: 4 // Größe des Genoms pro Kreatur
-  mutationRate: 0.001 // Mutationsrate (Beispiel: 0.001 bedeutet 0.1% Wahrscheinlichkeit für eine Mutation pro Gen)
-  innerNeurons: 3 // Anzahl der inneren Neuronen im Genom
-  speedInMs: 1000
+  secondsPerGeneration: number // Schritte pro Generation
+  genomeSize: number // Größe des Genoms pro Kreatur
+  mutationRate: number // Mutationsrate (Beispiel: 0.001 bedeutet 0.1% Wahrscheinlichkeit für eine Mutation pro Gen)
+  innerNeurons: number // Anzahl der inneren Neuronen im Genom
+  speedInMs: number
 }
