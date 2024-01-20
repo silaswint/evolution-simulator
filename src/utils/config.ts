52aa3218819eedeffe151a4b4ec2748154d711cb
silas.winter <silas.winter@p2media.de>
2024-01-20 16:01:24 +0100

    [TASK] example settings

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,10 +5,10 @@ import { CHALLENGE_RIGHT_SIDE_SURVIVES } from '@/utils/consts/challenges'
 export const config: EvolutionConfig = {
   population: 100,
   mapSize: { width: 800, height: 600 },
-  secondsPerGeneration: 10,
-  genomeSize: 64,
-  mutationRate: 0.001,
-  innerNeurons: 5,
+  secondsPerGeneration: 5,
+  genomeSize: 4,
+  mutationRate: 0.01,
+  innerNeurons: 1,
   speedInMs: 1000,
   challenge: CHALLENGE_RIGHT_SIDE_SURVIVES,
   movingSpeed: 3
