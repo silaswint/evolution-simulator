2d9a5da65901669330fb8704a3c625cf6bc270c2
silas.winter <silas.winter@p2media.de>
2024-01-20 23:50:25 +0100

    [TASK] added font awesome and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import HamsterModal from './components/HamsterModal'
 import fileDownload from 'js-file-download'
 import { generateRandomHamsters } from '@/utils/generateRandomHamsters'
 import { genomeToHex } from '@/utils/genome'
+import 'bootstrap'
+import 'bootstrap/scss/bootstrap.scss'
+import './custom.scss'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCakeCandles, faClock, faPeopleGroup } from '@fortawesome/free-solid-svg-icons'
 
 const mapSize = config.mapSize
 const App: React.FC = () => {
@@ -41,23 +47,31 @@ const App: React.FC = () => {
   }
 
   return (
-      <>
-          <h1>Evolution Simulation</h1>
-          <p>Generation: {generation}</p>
-          <p>Seconds left for current generation: {secondsLeftForCurrentGeneration}</p>
-          <p>Surviving population: {survivingPopulation} / {config.population}</p>
-          <p>Download current generation of hamsters:
-              <button onClick={() => {
-                downloadGeneration()
-              }}>Download</button>
-          </p>
-          <br />
-          <br />
+      <Container fluid className="mt-4" style={{ maxWidth: '800px' }}>
+          <h1 className="mb-4">Evolution Simulation</h1>
+          <Row className="mb-3">
+              <Col>
+                  <Row className="mb-3">
+                      <Col xs={6} md={2} className="mb-2 mb-md-0">
+                          <p className="mb-0" title={'Generation'}><FontAwesomeIcon icon={faCakeCandles} /> {generation}</p>
+                      </Col>
+                      <Col xs={6} md={2} className="mb-2 mb-md-0">
+                          <p className="mb-0" title={'Seconds left for current generation'}><FontAwesomeIcon icon={faClock} /> {secondsLeftForCurrentGeneration}</p>
+                      </Col>
+                      <Col xs={12} md={4}>
+                          <p className="mb-0" title={'Surviving population'}><FontAwesomeIcon icon={faPeopleGroup} /> {survivingPopulation}/{config.population}</p>
+                      </Col>
+                  </Row>
+              </Col>
+              <Col className="text-end">
+                  <Button variant="primary" onClick={downloadGeneration} title={'Download current generation of hamsters'}>Download</Button>
+              </Col>
+          </Row>
           <Stage width={mapSize.width} height={mapSize.height} options={{ backgroundColor: 0xeef1f5 }}>
               <Hamsters population={population} secondsLeftForCurrentGeneration={secondsLeftForCurrentGeneration} generation={generation} setSelectedHamster={setSelectedHamster} resetGenerationCountdown={resetGenerationCountdown} setGeneration={setGeneration} setSurvivingPopulation={setSurvivingPopulation} hamsters={hamsters} setHamsters={setHamsters}/>
           </Stage>
           <HamsterModal selectedHamster={selectedHamster} setSelectedHamster={setSelectedHamster} />
-      </>
+      </Container>
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
         extensions: [".tsx", ".ts", ".js", ".jsx"],
         alias: {
             "@": path.resolve(__dirname, "src"),
-        },
-    },
+        }
+},
     module: {
         rules: [
             {
@@ -34,6 +34,17 @@ module.exports = {
                         loader: "ts-loader",
                     },
                 ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ]
             },
         ],
     },
