854a58ed4de970b3338a82126343ce13cac72e92
silas.winter <silas.winter@p2media.de>
2024-01-20 18:01:02 +0100

    [TASK] disable warning for unused vars

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,15 @@
 // src/utils/evolutionConfig.ts
 import { type EvolutionConfig } from '@/utils/types/EvolutionConfig'
-import { CHALLENGE_RIGHT_SIDE_SURVIVES } from '@/utils/consts/challenges'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { CHALLENGE_NONE, CHALLENGE_RIGHT_SIDE_SURVIVES } from '@/utils/consts/challenges'
 
 export const config: EvolutionConfig = {
   population: 100,
   mapSize: { width: 800, height: 600 },
   secondsPerGeneration: 5,
-  genomeSize: 4,
-  mutationRate: 0.01,
-  innerNeurons: 1,
+  genomeSize: 40,
+  mutationRate: 0.001,
+  innerNeurons: 10,
   speedInMs: 1000,
   challenge: CHALLENGE_RIGHT_SIDE_SURVIVES,
   movingSpeed: 3
