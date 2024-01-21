51506421b7155ece2fb0f6301482198ff4e88594
silas.winter <silas.winter@p2media.de>
2024-01-21 20:53:58 +0100

    [REFACTOR] Improve formatting and readability of getRandomHamsterState function

--- src/utils/getRandomHamsterState.ts
+++ src/utils/getRandomHamsterState.ts
@@ -1,5 +1,5 @@
-import { type HamsterGeneratorResponse } from '@/utils/types/HamsterGeneratorResponse'
-import { type HamsterState } from '@/utils/types/HamsterState'
+import type { HamsterGeneratorResponse } from '@/utils/types/HamsterGeneratorResponse'
+import type { HamsterState } from '@/utils/types/HamsterState'
 import { defaultMovingSpeed } from '@/utils/consts/defaultMovingSpeed'
 
 export const getRandomHamsterState = (prev: HamsterState): HamsterGeneratorResponse => {
@@ -9,8 +9,8 @@ export const getRandomHamsterState = (prev: HamsterState): HamsterGeneratorRespo
   const newX = prev.x + prev.directionX * defaultMovingSpeed
   const newY = prev.y + prev.directionY * defaultMovingSpeed
 
-  const updatedDirectionX = ((newX === 0) ? -newDirectionX : newDirectionX) as 1 | -1 | 0
-  const updatedDirectionY = ((newY === 0) ? -newDirectionY : newDirectionY) as 1 | -1 | 0
+  const updatedDirectionX = (newX === 0 ? -newDirectionX : newDirectionX) as 1 | -1 | 0
+  const updatedDirectionY = (newY === 0 ? -newDirectionY : newDirectionY) as 1 | -1 | 0
 
   return {
     x: newX,
