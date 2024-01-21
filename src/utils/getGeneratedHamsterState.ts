09d5d4293646e7fa6c3675aa68e91aad04545abf
silas.winter <silas.winter@p2media.de>
2024-01-21 20:32:08 +0100

    [TASK] use immutable to map it readable

--- src/utils/getGeneratedHamsterState.ts
+++ src/utils/getGeneratedHamsterState.ts
@@ -1,25 +1,39 @@
 import { type HamsterGeneratorResponse } from '@/utils/types/HamsterGeneratorResponse'
 import { type HamsterState } from '@/utils/types/HamsterState'
+import { type SensoryInputs } from '@/utils/types/SensoryInputs'
+import { type MapSize } from '@/utils/types/MapSIze'
 import { getRandomHamsterState } from '@/utils/getRandomHamsterState'
 import { brain } from '@/utils/brain'
 import { config } from '@/utils/config'
 import { randomNumberBetween } from '@/utils/math/randomNumberBetween'
 import { mapValueToRange } from '@/utils/mapValueToRange'
 import { getDistanceToNearestHamster } from '@/utils/getDistanceToNearestHamster'
-import { type SensoryInputs } from '@/utils/types/SensoryInputs'
-import { type MapSize } from '@/utils/types/MapSIze'
+import { Map } from 'immutable'
 
-export const getGeneratedHamsterState = (prev: HamsterState, secondsLeftForCurrentGeneration: number, population: number, generation: number, prevHamsters: HamsterState[], mapSize: MapSize): HamsterGeneratorResponse => {
+export const getGeneratedHamsterState = (
+  prev: HamsterState,
+  secondsLeftForCurrentGeneration: number,
+  population: number,
+  generation: number,
+  prevHamsters: HamsterState[],
+  mapSize: MapSize
+): HamsterGeneratorResponse => {
+  const {
+    secondsPerGeneration,
+    mapSize: { width, height },
+    movingSpeed
+  } = config
+
   const distancesToOtherHamsters = getDistanceToNearestHamster(prev, prevHamsters)
 
   const sensoryInputs: SensoryInputs = {
-    age: config.secondsPerGeneration - secondsLeftForCurrentGeneration,
+    age: secondsPerGeneration - secondsLeftForCurrentGeneration,
     random: randomNumberBetween(-4, 4),
     currentPositionY: prev.y,
     currentPositionX: prev.x,
     generation,
-    sizeOfMapX: config.mapSize.width,
-    sizeOfMapY: config.mapSize.height,
+    sizeOfMapX: width,
+    sizeOfMapY: height,
     population,
     distanceOfNextObjectNorth: distancesToOtherHamsters.North,
     distanceOfNextObjectEast: distancesToOtherHamsters.East,
@@ -27,32 +41,19 @@ export const getGeneratedHamsterState = (prev: HamsterState, secondsLeftForCurre
     distanceOfNextObjectWest: distancesToOtherHamsters.West
   }
 
-  const normalizedSensoryInputs: SensoryInputs = {
-    age: Math.tanh(sensoryInputs.age),
-    random: Math.tanh(sensoryInputs.random),
-    currentPositionY: Math.tanh(sensoryInputs.currentPositionY),
-    currentPositionX: Math.tanh(sensoryInputs.currentPositionX),
-    generation: Math.tanh(sensoryInputs.generation),
-    sizeOfMapX: Math.tanh(sensoryInputs.sizeOfMapX),
-    sizeOfMapY: Math.tanh(sensoryInputs.sizeOfMapY),
-    population: Math.tanh(sensoryInputs.population),
-    distanceOfNextObjectNorth: Math.tanh(sensoryInputs.distanceOfNextObjectNorth),
-    distanceOfNextObjectEast: Math.tanh(sensoryInputs.distanceOfNextObjectEast),
-    distanceOfNextObjectSouth: Math.tanh(sensoryInputs.distanceOfNextObjectSouth),
-    distanceOfNextObjectWest: Math.tanh(sensoryInputs.distanceOfNextObjectWest)
-  }
+  const normalizedSensoryInputs: SensoryInputs = Map(sensoryInputs).map((v, k) => Math.tanh(v)).toJS() as unknown as SensoryInputs
 
   const brainResponse = brain(normalizedSensoryInputs, prev.genome)
 
   const newDirectionX = Math.sign(brainResponse.directionX)
   const newDirectionY = Math.sign(brainResponse.directionY)
 
-  const movingSpeed = mapValueToRange(brainResponse.movingSpeed, -1, 1, 0, config.movingSpeed)
-  const newX = prev.x + prev.directionX * movingSpeed
-  const newY = prev.y + prev.directionY * movingSpeed
+  const movingSpeedValue = mapValueToRange(brainResponse.movingSpeed, -1, 1, 0, movingSpeed)
+  const newX = prev.x + prev.directionX * movingSpeedValue
+  const newY = prev.y + prev.directionY * movingSpeedValue
 
-  const updatedDirectionX = ((newX === 0) ? -newDirectionX : newDirectionX) as 1 | -1 | 0
-  const updatedDirectionY = ((newY === 0) ? -newDirectionY : newDirectionY) as 1 | -1 | 0
+  const updatedDirectionX = (newX === 0 ? -newDirectionX : newDirectionX) as 1 | -1 | 0
+  const updatedDirectionY = (newY === 0 ? -newDirectionY : newDirectionY) as 1 | -1 | 0
 
   if (brainResponse.random > 0.5) {
     return getRandomHamsterState(prev)
