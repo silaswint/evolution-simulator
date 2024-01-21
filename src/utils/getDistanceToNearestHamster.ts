5ac5908d03f7bf6e4fd5bbd0be4776c4ed58ca61
silas.winter <silas.winter@p2media.de>
2024-01-21 21:15:59 +0100

    [REFACTOR] Improve calculation of distances and readability in getDistanceToNearestHamster function

--- src/utils/getDistanceToNearestHamster.ts
+++ src/utils/getDistanceToNearestHamster.ts
@@ -1,11 +1,11 @@
-import { type HamsterState } from '@/utils/types/HamsterState'
+import type { HamsterState } from '@/utils/types/HamsterState'
 
 export const getDistanceToNearestHamster = (currentHamster: HamsterState, otherHamsters: HamsterState[]): Record<string, number> => {
   const distances: Record<string, number> = {
-    North: Number.POSITIVE_INFINITY,
-    East: Number.POSITIVE_INFINITY,
-    South: Number.POSITIVE_INFINITY,
-    West: Number.POSITIVE_INFINITY
+    North: Infinity,
+    East: Infinity,
+    South: Infinity,
+    West: Infinity
   }
 
   for (const otherHamster of otherHamsters) {
@@ -14,7 +14,7 @@ export const getDistanceToNearestHamster = (currentHamster: HamsterState, otherH
       const deltaY = otherHamster.y - currentHamster.y
 
       // Calculate distance using Pythagorean theorem
-      const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2)
+      const distance = Math.hypot(deltaX, deltaY)
 
       // Check direction and update distance if it's shorter
       if (deltaY > 0 && distance < distances.North) {
