0e7a5ffd1375e5456faeb036573aebb95c0a0e39
silas.winter <silas.winter@p2media.de>
2024-01-21 21:09:47 +0100

    [REFACTOR] Improve formatting and readability in getFormattedDecimalGenome function

--- src/utils/getFormattedDecimalGenome.ts
+++ src/utils/getFormattedDecimalGenome.ts
@@ -1,5 +1,5 @@
-import { type Genome } from '@/utils/types/Genome'
-import { type Gene } from '@/utils/types/Gene'
+import type { Genome } from '@/utils/types/Genome'
+import type { Gene } from '@/utils/types/Gene'
 import { convertBase } from '@/utils/math/convertBase'
 import {
   NUM_ACTION_OUTPUT_NEURONS,
@@ -9,7 +9,7 @@ import {
   WEIGHT_FLOATING_POINT
 } from '@/utils/consts/brain'
 import { config } from '@/utils/config'
-import { type DecimalGenome } from '@/utils/types/DecimalGenome'
+import type { DecimalGenome } from '@/utils/types/DecimalGenome'
 import { genomeToHex } from '@/utils/genome'
 
 interface SplitResult {
@@ -22,30 +22,32 @@ const splitFirstCharacter = (str: string): SplitResult => {
     return { firstCharacter: null, restOfString: '' }
   }
 
-  const firstCharacter = str.charAt(0)
-  const restOfString = str.slice(1)
-
-  return { firstCharacter, restOfString }
+  const [firstCharacter, ...restOfString] = str.split('')
+  return { firstCharacter, restOfString: restOfString.join('') }
 }
 
 export const getFormattedDecimalGenome = (genome: Genome): DecimalGenome => {
-  return genome.map((gene, index) => {
-    const splittedFirstCharacterOfWeight = splitFirstCharacter(gene.weight)
-    const weight = convertBase.bin2dec(splittedFirstCharacterOfWeight.restOfString)
+  return genome.map((gene) => {
+    const { firstCharacter, restOfString } = splitFirstCharacter(gene.weight)
+    const weight = convertBase.bin2dec(restOfString)
 
     const decimalGene: Gene = {
       sourceType: convertBase.bin2dec(gene.sourceType),
       sourceId: convertBase.bin2dec(gene.sourceId),
       sinkType: convertBase.bin2dec(gene.sinkType),
       sinkId: convertBase.bin2dec(gene.sinkId),
-      weight: splittedFirstCharacterOfWeight.firstCharacter === '0' ? (-weight).toString() : weight
+      weight: firstCharacter === '0' ? (-weight).toString() : weight
     }
 
     return {
       sourceType: Number(decimalGene.sourceType),
-      sourceId: Number(decimalGene.sourceType) === SOURCE_TYPE_INPUT_INTERNAL_NEURON ? Number(decimalGene.sourceId) % config.innerNeurons : Number(decimalGene.sourceId) % NUM_SENSORY_NEURONS,
+      sourceId: Number(decimalGene.sourceType) === SOURCE_TYPE_INPUT_INTERNAL_NEURON
+        ? Number(decimalGene.sourceId) % config.innerNeurons
+        : Number(decimalGene.sourceId) % NUM_SENSORY_NEURONS,
       sinkType: Number(decimalGene.sinkType),
-      sinkId: Number(decimalGene.sinkType) === SINK_TYPE_INTERNAL_NEURON ? Number(decimalGene.sinkId) % config.innerNeurons : Number(decimalGene.sinkId) % NUM_ACTION_OUTPUT_NEURONS,
+      sinkId: Number(decimalGene.sinkType) === SINK_TYPE_INTERNAL_NEURON
+        ? Number(decimalGene.sinkId) % config.innerNeurons
+        : Number(decimalGene.sinkId) % NUM_ACTION_OUTPUT_NEURONS,
       weight: Number(decimalGene.weight) / WEIGHT_FLOATING_POINT,
       hex: genomeToHex([gene])
     }
