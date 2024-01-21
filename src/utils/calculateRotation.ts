718daf30ed4e00328a77b472555208b9ea306a98
silas.winter <silas.winter@p2media.de>
2024-01-21 22:06:22 +0100

    [REFACTOR] translate comments

--- src/utils/calculateRotation.ts
+++ src/utils/calculateRotation.ts
@@ -1,20 +1,20 @@
 export const calculateRotation = (directionX: 1 | -1 | 0, directionY: 1 | -1 | 0): number => {
-  // Wenn beide Richtungskomponenten 0 sind, gibt es keine Rotation
+  // If both direction components are 0, there is no rotation
   if (directionX === 0 && directionY === 0) {
     return 0
   }
 
-  // Berechne den Winkel im Bogenmaß
+  // Calculate the angle in radians
   const angleRad = Math.atan2(directionY, directionX)
 
-  // Wandele den Winkel von Bogenmaß in Grad um
+  // Convert the angle from radians to degrees
   let angleDeg = (angleRad * 180) / Math.PI
 
-  // Passe die Rotation an, um die Ausrichtung der Grafik zu berücksichtigen
-  // Beachte, dass Pixi.js 0 Grad als Ausrichtung nach rechts betrachtet
+  // Adjust the rotation to take the orientation of the graphic into account
+  // Note that Pixi.js considers 0 degrees as orientation to the right
   angleDeg += 90
 
-  // Stelle sicher, dass der Winkel im Bereich [0, 360) liegt
+  // Make sure that the angle is in the range [0, 360)
   angleDeg = (angleDeg + 360) % 360
 
   return angleDeg
