1aff7a516325f01be352610e00e949b31c71b1b6
silas.winter <silas.winter@p2media.de>
2024-01-21 20:46:51 +0100

    [REFACTOR] Improve code formatting, readability, and type imports
    - Enhanced indentation and line breaks for better code readability
    - Clarified type imports for HamsterState and MapSize
    - Updated comments to English for consistency in the code

--- src/utils/move.ts
+++ src/utils/move.ts
@@ -2,20 +2,34 @@ import { getGeneratedHamsterState } from '@/utils/getGeneratedHamsterState'
 import { hamsterSize } from '@/utils/consts/hamsterSize'
 import { isOverlap } from '@/utils/isOverlap'
 import { dontMove } from '@/components/Hamsters'
-import { type HamsterState } from '@/utils/types/HamsterState'
-import { type MapSize } from '@/utils/types/MapSIze'
+import type { HamsterState } from '@/utils/types/HamsterState'
+import type { MapSize } from '@/utils/types/MapSIze'
 import { calculateRotation } from '@/utils/calculateRotation'
 
-export const move = (prev: HamsterState, prevHamsters: HamsterState[], secondsLeftForCurrentGeneration: number, population: number, generation: number, mapSize: MapSize): HamsterState => {
+export const move = (
+  prev: HamsterState,
+  prevHamsters: HamsterState[],
+  secondsLeftForCurrentGeneration: number,
+  population: number,
+  generation: number,
+  mapSize: MapSize
+): HamsterState => {
   const { id, genome } = prev
-  const { x, y, directionX, directionY } = getGeneratedHamsterState(prev, secondsLeftForCurrentGeneration, population, generation, prevHamsters, mapSize)
+  const { x, y, directionX, directionY } = getGeneratedHamsterState(
+    prev,
+    secondsLeftForCurrentGeneration,
+    population,
+    generation,
+    prevHamsters,
+    mapSize
+  )
 
-  // This condition checks whether the hamster is within the limits
-  if (x < 0 || y < 0 || (x + hamsterSize.width) > mapSize.width || (y + hamsterSize.height) > mapSize.height) {
+  // Check if the hamster is within the map bounds
+  if (x < 0 || y < 0 || x + hamsterSize.width > mapSize.width || y + hamsterSize.height > mapSize.height) {
     return dontMove(prev, id, genome)
   }
 
-  // This conditions checks whether the location is empty
+  // Check if the location is empty
   if (isOverlap(x, y, prevHamsters, id)) {
     return dontMove(prev, id, genome)
   }
