b357ec007884b1322c0fe40703e8230061f12496
silas.winter <silas.winter@p2media.de>
2024-01-28 22:09:20 +0100

    [BUGFIX] make it more random

--- tests/mutation.test.ts
+++ tests/mutation.test.ts
@@ -2,11 +2,16 @@ import { generateRandomGenome, mutateGenome } from '@/utils/genome'
 import { config } from '@/utils/config/config'
 
 describe('mutateGenome', () => {
-  it('should keep the first element unchanged', () => {
+  it('should keep the mutated genome unchanged for 50 random genomes', () => {
     config.mutationRate = 0
-    const genome = generateRandomGenome()
-    const mutatedGenome = mutateGenome(genome)
 
-    expect(mutatedGenome).toStrictEqual(genome)
+    // Erstelle 50 zufällige Genome
+    for (let i = 0; i < 50; i++) {
+      const genome = generateRandomGenome()
+      const mutatedGenome = mutateGenome(genome)
+
+      // Vergleiche jedes mutierte Genome mit seinem Original
+      expect(mutatedGenome).toStrictEqual(genome)
+    }
   })
 })
