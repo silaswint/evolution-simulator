d884ad4a1cd1551eeb67f4c44d34fb3eb65be34f
silas.winter <silas.winter@p2media.de>
2024-01-28 16:48:19 +0100

    [EXPERIMENTAL] add config for multiple hidden layer

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,15 +1,15 @@
 // src/utils/evolutionConfig.ts
 import { type EvolutionConfig } from '@/utils/types/EvolutionConfig'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { CHALLENGE_NONE, CHALLENGE_RIGHT_SIDE_SURVIVES } from '@/utils/consts/challenges'
+import { CHALLENGE_NONE, CHALLENGE_RIGHT_SIDE_SURVIVES, CHALLENGE_RIGHT_SIDE_20_SURVIVES } from '@/utils/consts/challenges'
 
 export const config: EvolutionConfig = {
-  population: 20,
+  population: 50,
   mapSize: { width: 800, height: 600 },
   secondsPerGeneration: 5,
   genomeSize: 40,
   mutationRate: 0.001,
-  innerNeurons: 10,
   challenge: CHALLENGE_RIGHT_SIDE_SURVIVES,
   movingSpeed: 3
+  innerNeurons: [10, 5],
 }
