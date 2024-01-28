053ee0e0c66f12fdffbdaa10a49fb72d9035f551
silas.winter <silas.winter@p2media.de>
2024-01-28 23:08:57 +0100

    [TASK] add test for having no distance between to hamsters

---
+++ tests/noDistanceBetweenTwoHamsters.ts
@@ -0,0 +1,41 @@
+import { generateRandomGenome } from '@/utils/genome'
+import { getDistanceToNearestHamster } from '@/utils/hamsters/getDistanceToNearestHamster'
+import { type HamsterState } from '@/utils/types/HamsterState'
+import { getRandomDirection } from '@/utils/getRandomDirection'
+import { hamsterSize } from '@/utils/consts/hamsterSize'
+
+describe('noDistanceBetweenTwoHamsters', () => {
+  it('should find a distance because they should see each other even if they are not exactly in the same row', () => {
+    const hamster1: HamsterState = {
+      id: 0,
+      x: 50,
+      y: 50,
+      directionX: getRandomDirection(),
+      directionY: getRandomDirection(),
+      genome: generateRandomGenome(),
+      lastRotation: 0,
+      currentRotation: 0
+    }
+
+    const hamster2: HamsterState = {
+      id: 1,
+      x: 50,
+      y: 50 + (hamsterSize.height * 0.5),
+      directionX: getRandomDirection(),
+      directionY: getRandomDirection(),
+      genome: generateRandomGenome(),
+      lastRotation: 0,
+      currentRotation: 0
+    }
+
+    const otherHamsters = [hamster1, hamster2]
+    const distance = getDistanceToNearestHamster(hamster1, otherHamsters)
+
+    expect(distance).toStrictEqual({
+      North: 12.5,
+      East: Infinity,
+      South: Infinity,
+      West: Infinity
+    })
+  })
+})
