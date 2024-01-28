71e28d321684a732b7c9321515d4983311894ebb
silas.winter <silas.winter@p2media.de>
2024-01-28 22:07:23 +0100

    [BUGFIX] make mutationRate always zero for this test

--- tests/mutation.test.ts
+++ tests/mutation.test.ts
@@ -1,10 +1,12 @@
 import { generateRandomGenome, mutateGenome } from '@/utils/genome'
+import { config } from '@/utils/config/config'
 
 describe('mutateGenome', () => {
   it('should keep the first element unchanged', () => {
+    config.mutationRate = 0
     const genome = generateRandomGenome()
     const mutatedGenome = mutateGenome(genome)
 
-    expect(mutatedGenome[0]).toStrictEqual(genome[0])
+    expect(mutatedGenome).toStrictEqual(genome)
   })
 })
