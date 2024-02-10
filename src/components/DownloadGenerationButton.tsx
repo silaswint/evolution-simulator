60edb9086c32150e6507a79540ec2146a251393d
silas.winter <silas.winter@p2media.de>
2024-02-10 23:59:14 +0100

    [TASK] add config data to download json

--- src/components/DownloadGenerationButton.tsx
+++ src/components/DownloadGenerationButton.tsx
@@ -3,10 +3,21 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 import fileDownload from 'js-file-download'
 import { type HamsterState } from '@/utils/types/HamsterState'
+import { config } from '@/utils/config/config'
 
 const DownloadGenerationButton: React.FC<{ hamsters: HamsterState[] }> = ({ hamsters }) => {
   const downloadGeneration = (): void => {
-    fileDownload(JSON.stringify(hamsters.map(hamster => genomeToHex(hamster.genome))), 'generation.json')
+    const hexHamsters = hamsters.map(hamster => genomeToHex(hamster.genome))
+    const data = {
+      config: {
+        genomeSize: config.genomeSize,
+        innerNeurons: config.innerNeurons
+      },
+      hamsters: hexHamsters
+    }
+    const asJson = JSON.stringify(data)
+
+    fileDownload(asJson, 'generation.json')
   }
 
   return <Button variant="primary" onClick={downloadGeneration} title={'Download current generation of hamsters'}>Download</Button>
